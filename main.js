e50ba484378eab2e1411795853b29861987325f5
Honeshwar Thakur <honeshwar.thakur@tailnode.com>
2024-07-10 12:34:44 +0530

    add post webhook

--- main.js
+++ main.js
@@ -1,34 +1,13 @@
 // console.log("hi")
 import express, { urlencoded } from "express";//package.json type=module that by we were able to use import statement ES6
-import {config} from 'dotenv'//Loads environment variables from .env file
-config();
-const mytoken = process.env.WEBHOOK_VERIFY_TOKEN;
-console.log(mytoken);
-
+import {route} from './route/index.js'
 const PORT = 8000;
 const app = express();
 
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
+app.use("/",route)
 
-//to verify the callback url from dashboard side - cloud api side
-app.get('/webhook',(req,res)=>{
-    console.log("webhook called for verification by whatsapp API");
-    let mode=req.query["hub.mode"];
-    let challange=req.query["hub.challenge"];
-    let token=req.query["hub.verify_token"];
- 
- console.log(mode,challange,token);
-     if(mode && token){
- 
-         if(mode==="subscribe" && token===mytoken){
-             res.status(200).send(challange);
-         }else{
-             res.status(403);
-         }
- 
-     }
-})
 app.listen(PORT,(err)=>{
     if(err)
         return console.log("error while running to server...")
---
+++ route/index.js
@@ -0,0 +1,81 @@
+import { Router } from "express";//package.json type=module that by we were able to use import statement ES6
+import {config} from 'dotenv'//Loads environment variables from .env file
+config();
+const token = process.env.WHATSAPP_API_ACCESS_TOKEN;
+const mytoken = process.env.WEBHOOK_VERIFY_TOKEN;
+console.log(mytoken);
+
+export const route = Router();
+
+
+
+//to verify the callback url from dashboard side - cloud api side
+route.get('/webhook',(req,res)=>{
+    console.log("webhook called for verification by whatsapp API");
+    let mode=req.query["hub.mode"];
+    let challange=req.query["hub.challenge"];
+    let token=req.query["hub.verify_token"];
+ 
+ console.log(mode,challange,token);//subscribe 1890408503 apple
+     if(mode && token){
+ 
+         if(mode==="subscribe" && token===mytoken){
+             res.status(200).send(challange);
+         }else{
+             res.status(403);
+         }
+ 
+     }
+})
+route.post("/webhook",(req,res)=>{ //i want some 
+
+    let body_param=req.body;
+
+    console.log("body_param",JSON.stringify(body_param,null,2));
+
+    if(body_param.object){
+        console.log("inside body param");
+        if(body_param.entry && 
+            body_param.entry[0].changes && 
+            body_param.entry[0].changes[0].value.messages && 
+            body_param.entry[0].changes[0].value.messages[0]  
+            ){
+               let phon_no_id=body_param.entry[0].changes[0].value.metadata.phone_number_id;
+               let from = body_param.entry[0].changes[0].value.messages[0].from; 
+               let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+
+               console.log("phone number "+phon_no_id);
+               console.log("from "+from);
+               console.log("boady param "+msg_body);
+
+               axios({
+                   method:"POST",
+                   url:"https://graph.facebook.com/v20.0/"+phon_no_id+"/messages?access_token="+token,
+                   data:{
+                       messaging_product:"whatsapp",
+                       to:from,
+                       text:{
+                           body:"Hi.. I'm Prasath, your message is "+msg_body
+                       }
+                   },
+                   headers:{
+                       "Content-Type":"application/json"
+                   }
+
+               });
+
+               res.sendStatus(200);
+               return;
+            }else{
+                res.sendStatus(404);  return;
+            }
+           
+    }
+
+    res.sendStatus(404);
+
+});
+
+route.get("/",(req,res)=>{
+    res.status(200).send("hello this is webhook setup");
+});
