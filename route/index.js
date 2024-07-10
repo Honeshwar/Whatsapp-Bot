f99e005bda28b78781b532a279bcd493ebdbc3cf
Honeshwar Thakur <honeshwar.thakur@tailnode.com>
2024-07-10 13:08:22 +0530

    make it simple

--- route/index.js
+++ route/index.js
@@ -30,49 +30,52 @@ route.get('/webhook',(req,res)=>{
 route.post("/webhook",(req,res)=>{ //i want some 
 
     let body_param=req.body;
+    console.log("--------------------------");
+    console.log("body_param",body_param);
+    console.log("body_param stringify",JSON.stringify(body_param,null,2));
+    console.log("--------------------------");
+    // if(body_param.object){
+    //     console.log("inside body param");
+        // if(body_param.entry && 
+        //     body_param.entry[0].changes && 
+        //     body_param.entry[0].changes[0].value.messages && 
+        //     body_param.entry[0].changes[0].value.messages[0]  
+        //     ){
+        //        let phon_no_id=body_param.entry[0].changes[0].value.metadata.phone_number_id;
+        //        let from = body_param.entry[0].changes[0].value.messages[0].from; 
+        //        let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
 
-    console.log("body_param",JSON.stringify(body_param,null,2));
+        //        console.log("phone number "+phon_no_id);
+        //        console.log("from "+from);
+        //        console.log("boady param "+msg_body);
 
-    if(body_param.object){
-        console.log("inside body param");
-        if(body_param.entry && 
-            body_param.entry[0].changes && 
-            body_param.entry[0].changes[0].value.messages && 
-            body_param.entry[0].changes[0].value.messages[0]  
-            ){
-               let phon_no_id=body_param.entry[0].changes[0].value.metadata.phone_number_id;
-               let from = body_param.entry[0].changes[0].value.messages[0].from; 
-               let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+            //    axios({
+            //        method:"POST",
+            //        url:"https://graph.facebook.com/v20.0/"+phon_no_id+"/messages?access_token="+token,
+            //        data:{
+            //            messaging_product:"whatsapp",
+            //            to:from,
+            //            text:{
+            //                body:"Hi.. I'm Prasath, your message is "+msg_body
+            //            }
+            //        },
+            //        headers:{
+            //            "Content-Type":"application/json"
+            //        }
 
-               console.log("phone number "+phon_no_id);
-               console.log("from "+from);
-               console.log("boady param "+msg_body);
+            //    });
 
-               axios({
-                   method:"POST",
-                   url:"https://graph.facebook.com/v20.0/"+phon_no_id+"/messages?access_token="+token,
-                   data:{
-                       messaging_product:"whatsapp",
-                       to:from,
-                       text:{
-                           body:"Hi.. I'm Prasath, your message is "+msg_body
-                       }
-                   },
-                   headers:{
-                       "Content-Type":"application/json"
-                   }
-
-               });
-
-               res.sendStatus(200);
-               return;
-            }else{
-                res.sendStatus(404);  return;
-            }
+    //            res.sendStatus(200);
+    //            return;
+    //         }else{
+    //             res.sendStatus(404);  return;
+    //         }
            
-    }
+    // }
 
-    res.sendStatus(404);
+    // res.sendStatus(404);
+    res.sendStatus(200);
+    return;
 
 });
 
